3bb0d0a7313ce5750ae3eaed899c6318246dedfe
Kyle Dossett <kyle.dossett@norcalcontrols.net>
2024-03-25 17:11:37 -0400

    Updated to SNMP 3.7.8

--- norcal-snmp-driver/common/build.gradle.kts
+++ norcal-snmp-driver/common/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     // add common scoped dependencies here
     compileOnly("com.inductiveautomation.ignitionsdk:ignition-common:${rootProject.extra["sdk_version"]}")
     // https://mvnrepository.com/artifact/org.snmp4j/snmp4j
-	modlImplementation("org.snmp4j:snmp4j:3.4.1")
+	modlImplementation("org.snmp4j:snmp4j:3.7.8")
 }
